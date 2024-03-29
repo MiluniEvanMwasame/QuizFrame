7476190eb5689401d16cf7f18f736c6900933e20
MiluniEvanMwasame <164001921+MiluniEvanMwasame@users.noreply.github.com>
2024-03-29 20:57:45 +0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use '
+
 import type { Metadata } from 'next';
 import { getFrameMetadata } from "./utils/framesUtils";
 
@@ -9,10 +11,10 @@ const frameMetadata = getFrameMetadata({
 
 export const metadata: Metadata = {
   title: 'quiz frame',
-  description: 'Woodwork Fantasy',
+  description: 'QUIZFRAME',
   openGraph: {
     title: 'quiz frame',
-    description: 'Woodwork Fantasy',
+    description: 'QUIZFRAME',
     images: ['https://quiz-frame.vercel.app/titleImage.png'],
   },
   other: {
@@ -25,7 +27,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
-          <h1>Woodwork Fantasy</h1>
+          <h1>QUIZFRAME</h1>
         </div>
       </div>
     </main>
